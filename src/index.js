c9251da1af7d009e8bac1d23eca7f958ecd0d0ff
Mozzhakov <imozzhakov@icloud.com>
2022-12-18 21:35:37 +0200

    upd scroll

--- src/index.js
+++ src/index.js
@@ -18,28 +18,44 @@ async function onFormSubmit(e) {
   imagesApiService.query = e.currentTarget.elements.searchQuery.value;
   clearMarkup();
   imagesApiService.resetPage();
-  const arrOfImages = await imagesApiService.fetchImages();
-  createMarkup(renderImages(arrOfImages));
+  try {
+    const arrOfImages = await imagesApiService.fetchImages();
+    createMarkup(renderImages(arrOfImages));
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 0.25,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 async function onLoadMoreBtnClick() {
-  const arrOfImages = await imagesApiService.fetchImages();
-  createMarkup(renderImages(arrOfImages));
+  try {
+    const arrOfImages = await imagesApiService.fetchImages();
+    createMarkup(renderImages(arrOfImages));
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2.7,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function createMarkup(images) {
   refs.gallery.insertAdjacentHTML('beforeend', images);
   simpleGallery.refresh();
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
 }
 
 function clearMarkup() {
